0c041c7a1362d2b56230aa34734a3545e2b9f97d
AKASH MORE <101710919+0akash0@users.noreply.github.com>
2023-10-19 11:48:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World DevOps!')
+  response.send('Hello World DevOpsoooo!')
 })
 
 app.listen(app.get('port'), function() {
