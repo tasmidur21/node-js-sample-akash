7ea04bbe9dea8a2fc838c9a61465be1b5da491c0
tasmidur21 <tasmidur.rahman@byslglobal.com>
2024-04-15 18:37:23 +0600

    rizvi commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', function(request, response) {
   response.send('Hello World DevOpsoooo!')
 })
 
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
